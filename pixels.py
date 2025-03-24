c9fb5405347263cd6f932d9296463caf4e7ab1e0
Ronilerr <ronilerr123@gmail.com>
2025-03-24 23:07:58 +0200

    Update pixels.py

--- pixels.py
+++ pixels.py
@@ -1,23 +1,24 @@
 import cv2
 import os
 
-# נתיב לתיקיית התמונות
+# Path to the image folder
 input_folder = "./dataset/images"
 output_folder = "./dataset/processed_images"
 
-# יצירת תיקייה חדשה לתמונות המעובדות
+# Create a new folder for processed images
 os.makedirs(output_folder, exist_ok=True)
-# מעבר על כל התמונות בתיקייה
+
+# Loop through all images in the folder
 for filename in os.listdir(input_folder):
-        if filename.endswith(".jpg"):
-            img_path = os.path.join(input_folder, filename)
-            img = cv2.imread(img_path)
+    if filename.endswith(".jpg"):
+        img_path = os.path.join(input_folder, filename)
+        img = cv2.imread(img_path)
 
-            # שינוי גודל התמונה ל-224x224
-            img_resized = cv2.resize(img, (224, 224))
+        # Resize the image to 224x224
+        img_resized = cv2.resize(img, (224, 224))
 
-            # שמירה של התמונה המעובדת
-            output_path = os.path.join(output_folder, filename)
-            cv2.imwrite(output_path, img_resized)
+        # Save the processed image
+        output_path = os.path.join(output_folder, filename)
+        cv2.imwrite(output_path, img_resized)
 
 print(img_resized)
